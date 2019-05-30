5165db84b8fc5548e06da14f5b23a2b11c952985
Gordon Stangler <gordon.stangler@gmail.com>
2019-05-30 09:49:33 -0400

    Update BlackHoles.hpp
    This is going to change, significantly.

---
+++ Objects/BlackHoles.hpp
@@ -0,0 +1,62 @@
+#include <math>
+#include <complex>
+
+using namespace GRLib
+{
+	class BlackHole
+	{
+		public:
+		//variables, then functions
+		double radius;
+		//mass, spin, charge
+		//photon_sphere  //should this be a function?
+
+		//default M=0, S=0, Q=0
+		//need radius, ISCO, virtual functions for other derivable properties
+		Blackhole()  //constructor
+		{
+			double Mass = 0.0;
+			double Spin = 0.0;
+			double Charge = 0.0;
+		};
+
+		Blackhole(double Mass, double Spin, double Charge)
+		{ }
+
+		~Blackhole(){}  //destructor
+
+		double ISCO()
+		{
+			double r = (6*G_N * M)/(c^2);
+			return r;
+		}
+		protected:
+		private:
+	};
+
+	class Schwartzchild : Blackhole
+	{
+		public:
+		Schwartzchild(double Mass = 0.0)
+		{}
+		double ISCO()
+		{}
+		double photon_sphere()
+		{  return (1.5)*r_s;  }
+		protected:
+		private:
+	};
+
+	class Kerr : Blackhole
+	{
+		public:
+		Kerr(Mass = 0.0, Spin = 0.0)
+		{  }
+		double ISCO()
+		double photon_sphere()  {}  //need to find angular momentum, make that private?
+		protected:
+		private:
+	};
+	class RN
+	class //rotating, charged, mass
+}
