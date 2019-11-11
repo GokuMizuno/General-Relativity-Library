df482937d962d5591ba1df5414489b923c64db45
Gordon Stangler <gordon.stangler@gmail.com>
2019-11-10 23:48:26 -0500

    Update Matrix.h
    removing namespace std, making vector to std::vector.  Need to look for other std:: objects.

--- Objects/Matrix.h
+++ Objects/Matrix.h
@@ -17,28 +17,57 @@ class Matrix
 				thisRow.resize(columns);
 		}
 
-		matrix(vector<vector<Object>> v : array{v} {}
-		matrix(vector<vector<Object>> &&v : array{std::move(v)} {}
-		const vector<Object> & operator [](int row) const {  return array[row];  }
-		const vector<Object> & operator [](int row) {  return array[row];  }
+		matrix(std::vector<std::vector<Object>> v : array{v} {}
+		matrix(std::vector<std::vector<Object>> &&v : array{std::move(v)} {}
+		const std::vector<Object> & operator [](int row) const {  return array[row];  }
+		const std::vector<Object> & operator [](int row) {  return array[row];  }
 		int numRows() const {  return array.size();  }
 		int numCols() const {  return numRows() ? array[0].size : 0;  }
+
+		/*An identity matrix is one that has the identity element down the diagonal
+		  but zero everywhere else.  For R^n, I = 1.*/
 		matrix Id(int dim)
 		{
+			//make sure the matrix actually exists, and memory is allocated.
+			static_assert(this, matrix(dim, dim));
 			#pragma omp parallel
 			for (int i = 0; i < dim; i++)
 			{
-				for (j = 0; i < dim; j++)
+				for (int j = 0; i < dim; j++)
 				{
 					if (i == j)
-						this.[i][i] = 1;
+						this[i][i] = 1;
 					else
-						continue;
+						this[i][j] = 0;
 				}
 			}
 		}
 
+		matrix Id(int row, int col)
+		{
+			//construct matrix
+			mat = new matrix(row, col);
+			//then
+			#pragma omp parallel
+			for (int i = 0; i < row, i++)
+			{
+				for (int j = 0; j < col; j++)
+					mat[i][j] = 0;
+			}
+			return mat;
+		}
+
+		//matrix diag(int dim = 4, ...)
+		//{
+		//	;
+		//}
+		//matrix diag(std::vector<Object> v)
+		//{
+		//	//put at diags
+		//	for(auto i : v)
+		//}
+
 	private:
-		vector<vector<Object>> array;
+		std::vector<std::vector<Object>> array;
 };
 #endif
