4a0da163d8fb95d2f7ce45e96a1fc401511d3ecc
Gordon Stangler <gordon.stangler@gmail.com>
2021-04-15 09:49:35 -0400

    Update Constants.h
    Added a few more constants

--- Objects/Constants.h
+++ Objects/Constants.h
@@ -84,19 +84,22 @@ namespace GRLib
 		//default:
 			const double c = 3.0E8;  //m/s
 			const double G = 6.67430E-11;  //(N*m^2)/kg^2
-			const double epsilon_0 = ;
+			const double epsilon_0 = 8.854187817E-12;  //C^2/N
+			const double k_B = 1.380649E-23;  //m^2*kg/s^2*K  //Boltzmann constant
 
 			//Quantum
-			const double h;
-			const double h_bar;
-			//plank length, mass, area
-			const double m_P = ;
-			const double l_P = ;
-			const double a_P = ;
+			const double h = 6.626E-34;
+			const double h_bar = h/(2*pi);  //TEST THIS!
+			//plank length, mass, time, temperature, area
+			const double m_P = sqrt((h_bar*c)/(G));
+			const double l_P = sqrt((h_bar*G)/(c*c*c));
+			const double t_P = sqrt((h_bar*G)/(c*c*c*c*c));
+			const double T_P = sqrt((h_bar*c*c*c*c*c) / (G*k_B));
+			const double a_P = l_P * l_P;
 
 			//Solar mass, radius
-			m_Sun = ;
-			r_Sun = ;
+			m_Sun = 1.989E30;  //kg
+			r_Sun = 696.34E6;  //meters
 
 			//Cosmological
 			const double lambda = ;
